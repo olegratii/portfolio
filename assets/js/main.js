f86b3e0d2c2bb70592f6e0fec664ad0c4b16794c
olegratii <olegratii@gmail.com>
2016-10-11 01:05:16 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,13 @@
 jQuery(document).ready(function($) {
-    
-   $(".exp-player").click(function(){
-	   $(".player-wrapper").toggleClass("hide-block");
-	   $(".player-wrapper").toggleClass("show-block");
+   
+   $(".section-label").click(function(){
+	   $(this).siblings(".section-wrapper").toggleClass("hide-block");
+	   $(this).siblings(".section-wrapper").toggleClass("show-block");
    })
-   $(".coach-exp").click(function(){
-	   $(".coach-wrapper").toggleClass("hide-block");
-	   $(".coach-wrapper").toggleClass("show-block");
-   })
-    $(".edu-title").click(function(){
-	   $(".education").toggleClass("hide-block");
-	   $(".education").toggleClass("show-block");
-   })
-   $(".add-title").click(function(){
-	   $(".add-training").toggleClass("hide-block");
-	   $(".add-training").toggleClass("show-block");
+   
+   $(".image-label").click(function(){
+	   $(this).siblings(".asset-img").toggleClass("hide-block");
+	   $(this).siblings(".asset-img").toggleClass("show-block");
    })
 
 });
