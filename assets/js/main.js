75fd33f830e3f16c5a5f165b8d66bc44af4f1100
Anton Ratiy <Anton_Ratiy@epam.com>
2017-08-11 16:47:06 +0300

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($) {
-   
+
    $(".section-label").click(function(){
 	   $(this).siblings(".section-wrapper").toggleClass("hide-block");
 	   $(this).siblings(".section-wrapper").toggleClass("show-block");
    })
-   
+
    $(".image-label").click(function(){
 	   $(this).siblings(".asset-img").toggleClass("hide-block");
 	   $(this).siblings(".asset-img").toggleClass("show-block");
